3dc486b337354a4dd49ff7489b7d696ee9d4fa0b
ayoublaw <akram_metallaoui@hotmail.com>
2018-11-07 22:12:48 +0100

    Test Postgre

--- src/main/java/Util/HibernateUtil.java
+++ src/main/java/Util/HibernateUtil.java
@@ -1,28 +1,46 @@
 package Util;
 
 import org.hibernate.SessionFactory;
-import org.hibernate.boot.Metadata;
-import org.hibernate.boot.MetadataSources;
-import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+
+import java.net.URI;
 
 public class HibernateUtil {
     private static SessionFactory buildSessionFactory() {
         try {
-            StandardServiceRegistry standardRegistry = new StandardServiceRegistryBuilder()
-                    .configure( "hibernate.cfg.xml" )
-                    .build();
+            Configuration configuration = new Configuration().configure();
+            final String DATABASE_URL = System.getenv("DATABASE_URL");
+            if (DATABASE_URL != null && !DATABASE_URL.isEmpty()) {
 
-            Metadata metadata = new MetadataSources( standardRegistry )
-                    .getMetadataBuilder()
-                    .build();
+                URI dbUri = new URI(DATABASE_URL);
+                String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+                String username = dbUri.getUserInfo().split(":")[0];
+                String password = dbUri.getUserInfo().split(":")[1];
+                System.out.println(dbUrl);
+                configuration.setProperty("hibernate.connection.username", username);
+                configuration.setProperty("hibernate.connection.password", password);
+                configuration.setProperty("hibernate.connection.url", dbUrl);
+                configuration.setProperty("connection.username", username);
+                configuration.setProperty("connection.password", password);
+                configuration.setProperty("connection.url", dbUrl);
 
-            return metadata.getSessionFactoryBuilder().build();
+                configuration.getProperties().list(System.out);
+            }
 
+            StandardServiceRegistryBuilder registry = new StandardServiceRegistryBuilder();
+            registry.applySettings(configuration.getProperties());
+            ServiceRegistry serviceRegistry = registry.build();
+
+            return configuration.buildSessionFactory(serviceRegistry);
+
+        } catch (Throwable ex) {
+            // Make sure you log the exception, as it might be swallowed
+            System.err.println("Initial SessionFactory creation failed." + ex);
+            throw new ExceptionInInitializerError(ex);
         }
-        catch (Throwable E){
-            throw new ExceptionInInitializerError(E);
-        }
+
     }
     public static SessionFactory getSessionFactory(){
         return buildSessionFactory();
